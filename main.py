d081412c96b490fff861a512a54b9e74c14946e5
relias08 <relias08@gmail.com>
2023-07-18 18:46:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# This file corresponds to following tutorial on YT from DevOps Journey:
+# [Github Actions CI/CD - Everything you need to know to get started](https://www.youtube.com/watch?v=mFFXuXjVgkU)
+
+
 def hello():
     print("hi")
 
